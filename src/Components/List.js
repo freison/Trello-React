c7d35f0a1a4365fb3a6b4f4dda984443bd94429a
freison <freisoncastro@gmail.com>
2020-08-31 17:22:25 -0600

    List.js

---
+++ src/Components/List.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Paper, Typography} from "@material-ui/core";
+
+export default function List(){
+    return(
+        <div>
+            <Paper>
+                <Typography>
+                    To do
+                </Typography>
+            </Paper>
+        </div>
+    );
+}
+
+export default List();
