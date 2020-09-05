58317863831fce62e3c9c5a7c086cdfc0cfff118
freison <freisoncastro@gmail.com>
2020-09-05 17:43:05 -0600

    Card.js

--- github.sh
+++ github.sh
@@ -5,18 +5,12 @@ github(){
 	git add .;
 	git status;
 	git commit -m $1;
-	# git push;
-	# git status;
+
+	if $2;
+	then
+		git push;
+		git status;
+	fi
 }
 
-githubPush(){
-	git push;
-	git status;
-}
-
-github $1
-
-if $2;
-then
-	githubPush;
-fi
+github $1 $2
